e825b187476797cfc95efb56b81f71f77bc060c7
Matt Hewitt <fluentin44languages@gmail.com>
2022-05-15 15:41:31 +0100

    Changed slurm prefs

--- jobfiles/slurm_jobfile.sh
+++ jobfiles/slurm_jobfile.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-#SBATCH --job-name=<NAME OF JOB>            # Name of job - can be anything
-#SBATCH --ntasks-per-node=1                 # Tasks per node
-#SBATCH --mem=30G                           # Memory required
-#SBATCH --nodes=1                           # Number of nodes requested (always 1)
-#SBATCH --mail-type=ALL                     # mail alert at start, end and abortion of execution
-#SBATCH --mail-user=moh1r17@soton.ac.uk     # send mail to this address
-#SBATCH --time=60:00:00                     # walltime - time needed for job - 60:00:00 = 60 hours, max time allowed
+#SBATCH --job-name=<NAME OF JOB>                    # Name of job - can be anything
+#SBATCH --ntasks-per-node=1                         # Tasks per node
+#SBATCH --mem=60G                                   # Memory required
+#SBATCH --nodes=1                                   # Number of nodes requested (always 1)
+#SBATCH --mail-type=ALL                             # mail alert at start, end and abortion of execution
+#SBATCH --mail-user=<YOUR USERNAME>@soton.ac.uk     # send mail to this address
+#SBATCH --time=60:00:00                             # walltime - time needed for job - 60:00:00 = 60 hours, max time allowed
 #SBATCH --output=output_files/slurm-%j.out
 #SBATCH --error=error_files/slurm-%j.err
 
